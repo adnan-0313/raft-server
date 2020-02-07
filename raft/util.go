8c59518d73a51d9798af642d2fb82b16c4d6d0fb
Shekh Ataul <ataul.one@outlook.com>
2020-02-07 11:28:13 +0530

    raft: util: added some useful functions

--- raft/util.go
+++ raft/util.go
@@ -1,13 +1,61 @@
 package raft
 
-import "log"
+import (
+	crand "crypto/rand"
+	"fmt"
+	"math/rand"
+	"time"
+)
 
-// Debugging
-const Debug = 0
+// randomTimeout returns a value in range [val, 2*val)
+func randomTimeout(val time.Duration) <-chan time.Time {
+	delta := time.Duration(rand.Int63()) % val
+	return time.After(val + delta)
+}
 
-func DPrintf(format string, a ...interface{}) (n int, err error) {
-	if Debug > 0 {
-		log.Printf(format, a...)
+// min returns the minimum.
+func min(a, b uint64) uint64 {
+	if a <= b {
+		return a
 	}
-	return
+	return b
+}
+
+// max returns the maximum
+func max(a, b uint64) uint64 {
+	if a >= b {
+		return a
+	}
+	return b
+}
+
+func generateUUID() string {
+	buf := make([]byte, 16)
+	if _, err := crand.Read(buf); err != nil {
+		panic(fmt.Errorf("failed to read random bytes: %v", err))
+	}
+
+	return fmt.Sprintf("%08x-%04x-%04x-%04x-%12x",
+		buf[0:4],
+		buf[4:6],
+		buf[6:8],
+		buf[8:10],
+		buf[10:16])
+}
+
+// asyncNotify is used to do an async channel send to
+// a list of channels. This will not block.
+func asyncNotify(chans []chan struct{}) {
+	for _, ch := range chans {
+		asyncNotifyCh(ch)
+	}
+}
+
+// asyncNotifyCh is used to do an async channel send
+// to a single channel without blocking.
+func asyncNotifyCh(ch chan struct{}) {
+	select {
+	case ch <- struct{}{}:
+	default:
+	}
 }
