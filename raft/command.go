bb86d4d9af38ed0612ab3eef486063a2805094ff
Shekh Ataul <ataul.one@outlook.com>
2020-02-07 12:36:23 +0530

    raft: command: added requestvote args and resp

--- raft/command.go
+++ raft/command.go
@@ -1,1 +1,23 @@
 package raft
+
+type RequestVoteArgs struct {
+	// Candidate's Term
+	Term uint64
+
+	// Candidate's ID
+	CandidateID uint64
+
+	// Index of candidate's last log entry
+	LastLogIndex uint64
+
+	// Term of candidate's last log entry
+	LastLogTerm uint64
+}
+
+type RequestVoteResp struct {
+	// Current term for candidate to update itself
+	Term uint64
+
+	// Vote received status
+	VoteGranted bool
+}
